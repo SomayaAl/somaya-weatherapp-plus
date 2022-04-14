630851fa3f660fd069a8e7d6d63dc449d92638d5
SomayaAl <102401317+SomayaAl@users.noreply.github.com>
2022-04-14 17:19:41 -0400

    Updated JS file

---
+++ src/script.js
@@ -0,0 +1,73 @@
+function searchForm(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-text-input").value;
+  let apiKey = "7d81cb66d2a78969cfec2f704335508f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+let cityForm = document.querySelector("#find-weather");
+
+cityForm.addEventListener("submit", searchForm);
+
+let now = new Date();
+let h3 = document.querySelector("h3");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hours = String(now.getHours()).padStart(2, "0");
+let minutes = String(now.getMinutes()).padStart(2, "0");
+
+h3.innerHTML = `${day} ${hours}:${minutes}`;
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let tempElement = document.querySelector("#temp");
+  tempElement.innerHTML = 75;
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let tempElement = document.querySelector("#temp");
+  tempElement.innerHTML = 23;
+}
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
+
+function searchLocation(position) {
+  let apiKey = "7d81cb66d2a78969cfec2f704335508f";
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function displayWeather(response) {
+  document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = response.data.wind.speed;
+  document.querySelector("#description").innerHTML =
+    response.data.weather.description;
+}
+
+let currentButton = document.querySelector("#current-location-button");
+currentButton.addEventListener("click", getCurrentLocation);
